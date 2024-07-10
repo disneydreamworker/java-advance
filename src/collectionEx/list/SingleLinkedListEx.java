487995c2762a222689a5590413cca592d73d7d4e
thatzcool <mycaptain0771@gmail.com>
2024-07-10 16:38:43 +0900

    java-advance-collection_singleLinkedList

--- src/collectionEx/list/SingleLinkedListEx.java
+++ src/collectionEx/list/SingleLinkedListEx.java
@@ -3,17 +3,23 @@ package collectionEx.list;
 public class SingleLinkedListEx {
 
   public static void main(String[] args) {
-       Node<Integer>  node1 = new Node<Integer>(1);
-       Node<Integer>  node2 = new Node<Integer>(2);
-       node1.next = node2;
-       Node<Integer> head = node1;
+    Node<Integer> node1 = new Node<Integer>(1);
+    Node<Integer> node2 = new Node<Integer>(2);
+    node1.next = node2;
+    Node<Integer> head = node1;
 
-       SingleLinkedList<Integer> mylist = new SingleLinkedList<>();
-       mylist.addNode(1);
-       mylist.addNode(2);
-       mylist.addNode(3);
-       //System.out.println(mylist.head.next.data);
+    SingleLinkedList<Integer> mylist = new SingleLinkedList<>();
+    mylist.addNode(1);
+    mylist.addNode(2);
+    mylist.addNode(3);
+    //System.out.println(mylist.head.next.data);
+    mylist.addNodeInside(5, 1);
     mylist.printAll();
+    mylist.delNode(3);
+
+    mylist.printAll();
+
+
   }
 
 }
