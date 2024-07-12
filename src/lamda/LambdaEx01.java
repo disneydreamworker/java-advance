fb29c9107e8c48ffd53fae9afbc4f57ffa87d7a9
thatzcool <mycaptain0771@gmail.com>
2024-07-12 16:10:52 +0900

    java-advance-Lambda_01

---
+++ src/lamda/LambdaEx01.java
@@ -0,0 +1,24 @@
+package lamda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaEx01 {
+
+  public static void main(String[] args) {
+    // List 컬렉션에 1,2,3,4,5,6,7,8,9,10 10개의 정수를 저장하고
+    // 컬렉션의 짝수의 합을 구하세요
+
+    List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+    int sumOfEvenNumbers = numbers.stream().filter(number->number%2 == 0)
+        .mapToInt(Integer::intValue).sum();
+
+    System.out.println(sumOfEvenNumbers);
+
+
+
+
+
+  }
+}
---
+++ src/lamda/LamdbaEx03.java
@@ -0,0 +1,19 @@
+package lamda;
+
+public class LamdbaEx03 {
+  /*
+   *  1.  정수 리스트를 받아 정수를 반환하는 단일 추상 메서드 getMax()를 사용하여 FindMax 인터페이스를 생성하세요
+   *  2.  정수 리스트를 반복하고 최대값을 찾는 람다식을 구현합니다.
+   *  3.  최대값은 Integer.MIN_VALUE 로 설정 한 후 더 놓은 숫자가 나타나면 업데이트 합니다.
+   *      최대값은 변수 max에 저장 합니다.
+   *  4.  정수리스트를 생성합니다.
+   *  5.  max 객체에서 getMax() 호출하여 최대값을 출력합니다.
+   * */
+
+
+  public static void main(String[] args) {
+
+  }
+
+}
+
